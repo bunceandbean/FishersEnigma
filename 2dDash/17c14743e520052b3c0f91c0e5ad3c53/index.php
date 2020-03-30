a4a56a70828920daae6ea801672bf989c70b2305
Ben <fishersengima@gmail.com>
2020-03-30 08:36:36 -0400

    New area

--- 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
+++ 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
@@ -102,7 +102,7 @@ usemap="#planetmap" id = "script">
                    document.getElementById("scrabble").style.display="block";
                }
 
-          document.querySelector("area").coords = "" + $(window).width() * .3234375 + "," + $(window).height() * .3234375 + ", 50";
+          document.querySelector("area").coords = "" + $(window).width() * .5486111 + "," + $(window).height() * .3234375 + ", 50";
 
 
    </script>
