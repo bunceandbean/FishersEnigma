0bbba3a970b534c7ec63309277f3c39d0ce2db9a
Ben <fishersengima@gmail.com>
2020-03-30 08:19:23 -0400

    jQuery Update

--- 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
+++ 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
@@ -37,6 +37,7 @@ if(isset($_POST["user"])){
      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js?n=1"></script>
      <style media="screen">
      video{
        position:fixed;
