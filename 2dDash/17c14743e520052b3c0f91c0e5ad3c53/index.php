6a3e3ad94780e35fcf382f731e9c715cdfdfe534
Ben <fishersengima@gmail.com>
2020-03-30 08:01:56 -0400

    Map Fixes

--- 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
+++ 2dDash/17c14743e520052b3c0f91c0e5ad3c53/index.php
@@ -87,7 +87,7 @@ map{
   <img src="place.jpg" alt="Planets"
 usemap="#planetmap" id = "#script">
 
-<map name="planetmap">
+<map name="planetmap" id = "#script">
   <area target="_blank" alt="" title="" href="book.html" coords="400,395,414,414">
 </map>
 </div>
