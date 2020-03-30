0cd614a842b9f04b636e7d0b047702e799d25d28
Ben <fishersengima@gmail.com>
2020-03-30 10:00:07 -0400

    Stage 3 leaderboard testing

---
+++ 2dDash/c77cedc6e07ddcf3ba23b4307160e393/index.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST["user"])){
+  $user = $_POST["user"];
+  $link = mysqli_connect("localhost", "fishbpzf_admin", "b3b57af55c7a898bcfff2732ae06660e", "fishbpzf_2dDash");
+  if($link === false){
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+  }
+  $sql = "SELECT * FROM leaderboard";
+  if($result = mysqli_query($link, $sql)){
+      if(mysqli_num_rows($result) > 0){
+
+          while($row = mysqli_fetch_array($result)){
+                  // echo "<td>" . $row['Team'] . "</td>";
+                  // echo "<td>" . $row['Stage'] . "</td>";
+                if($row['Stage'] <= 3){
+                  $update = "UPDATE `leaderboard` SET `Stage`=3 WHERE `Team` = '".$user."'";
+                  mysqli_query($link,$update);
+              }
+            }
+          }
+        }
+
+}
+
+
+ ?>
