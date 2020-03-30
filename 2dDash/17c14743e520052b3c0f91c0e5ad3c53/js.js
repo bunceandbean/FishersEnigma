37459c13e1488b7aec2c05c3a2271e8b3373412b
Ben <fishersengima@gmail.com>
2020-03-30 09:03:06 -0400

    cmon man

--- 2dDash/17c14743e520052b3c0f91c0e5ad3c53/js.js
+++ 2dDash/17c14743e520052b3c0f91c0e5ad3c53/js.js
@@ -1,1 +1,21 @@
-var _cs=["\x63\x6c","\x41\x44","\x74\x20\x6e","\x61\x79","\x6f\x74\x61","\x73\x20","\x64\x65","\x65\x61","\x6e\x6f\x6e","\x66\x6f\x72","\x61\x72","\x42\x54","\x65\x20",'\x77\x68\x69\x6c\x65','\x67\x65\x74',"\x6c\x65\x2e",'\x77\x69\x6e\x64\x6f\x77',"\x73\x63","\x73\x70\x6c","\x20\x52\x45","\x48\x69","\x20\x57\x49","\x6d\x6f\x73","\x2e\x2e","\x42\x79","\x74\x69\x74","\x56\x69","\x72\x61\x62","\x61\x72\x65",'\x77\x69\x6e\x64\x6f\x77','\x77\x68\x69\x6c\x65',"\x63\x6b","\x20\x54\x4f","\x6d\x79","\x44\x44\x42","\x62\x6c",'\x77\x68\x69\x6c\x65',"\x62\x6c\x6f","\x69\x70",'\x45\x6c\x65\x6d\x65\x6e\x74',"\x69\x63\x6b","\x28\x29","\x2c\x20\x31","\x74\x69\x6d\x65",'\x77\x69\x6e\x64\x6f\x77',"\x64\x69","\x73\x63\x72",'\x49\x64',"\x62\x36\x34",'\x61\x62\x73']; $(_cs[10]+_cs[7]).on(_cs[0]+_cs[40], function(_p0){ _p0.preventDefault(); _f0(); }); document.querySelector(_cs[28]+"a").onclick = _cs[45]+_cs[18]+_cs[3]+_cs[41]; function _f2(){ setTimeout(hideDiv, 8000); } function _f1() { document.getElementById(_cs[33]+_cs[26]+_cs[6]+"o").style.display=_cs[8]+"e"; document.getElementById(_cs[17]+_cs[27]+_cs[35]+"e").style.display=_cs[37]+_cs[31]; document.getElementById(_cs[46]+_cs[38]+"t").style.display=_cs[37]+_cs[31]; } document.querySelector(_cs[28]+"a").coords = "" + $(window).width() * .3234 + "," + $(window).height() *.5486111 + _cs[42]+"0"; function _f0(){ alert(_cs[20]+_cs[5]+_cs[22]+_cs[2]+_cs[4]+_cs[35]+_cs[12]+_cs[25]+_cs[15]+_cs[23]); alert(_cs[34]+_cs[11]+_cs[19]+_cs[1]+_cs[32]+_cs[21]+"N"); }
+var cs=["\x2c\x20\x31","\x44\x44\x42","\x61\x62\x62","\x65\x2e\x2e","\x77\x69\x6e\x64\x6f\x77","\x61\x72\x65","\x61","\x79\x28","\x42\x79",'\x31\x30\x32\x34',"\x61\x72","\x64\x69\x73","\x62\x6c","\x6e\x6f",'\x45\x6c\x65\x6d\x65\x6e\x74',"\x42\x54\x20","\x66\x75\x6e\x63\x74\x69\x6f\x6e","\x69\x64","\x69\x74\x6c","\x52\x45\x41","\x69\x70","\x63\x6b","\x2e","\x4e","\x57\x49","\x6f\x73","\x73\x63\x72","\x44\x20","\x20\x74","\x74","\x62\x6c\x6f","\x48\x69","\x63\x6c\x69","\x6e\x65","\x70\x6c\x61","\x74\x61\x62","\x6f\x63\x6b","\x77\x68\x69\x6c\x65","\x6c\x65","\x65\x6f","\x73\x20\x6d",'\x6d\x61\x74\x68',"\x6d\x79\x56","\x54\x4f\x20","\x74\x20","\x30","\x2c","\x65\x61","\x29"];    $(cs[5]+cs[6]).on(cs[32]+cs[21], function(p0){
+    p0.preventDefault();
+    f0();
+});
+   document.querySelector(cs[10]+cs[47]).onclick = cs[11]+cs[34]+cs[7]+cs[48];
+          function f2(){
+               setTimeout(hideDiv, 8000);
+             }
+
+               function f1() {
+                   document.getElementById(cs[42]+cs[17]+cs[39]).style.display=cs[13]+cs[33];
+                   document.getElementById(cs[26]+cs[2]+cs[38]).style.display=cs[12]+cs[36];
+                   document.getElementById(cs[26]+cs[20]+cs[29]).style.display=cs[30]+cs[21];
+
+               }
+
+          document.querySelector(cs[5]+cs[6]).coords = "" + $(window).width() * .3234 + cs[46] + $(window).height() *.5486111 + cs[0]+cs[45];
+          function f0(){
+            alert(cs[31]+cs[40]+cs[25]+cs[44]+cs[13]+cs[35]+cs[38]+cs[28]+cs[18]+cs[3]+cs[22]);
+            alert(cs[1]+cs[15]+cs[19]+cs[27]+cs[43]+cs[24]+cs[23]);
+          }
