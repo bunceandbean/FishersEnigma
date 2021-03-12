48709f067228594598b0e16492a751615c3268b2
Ben <benlilley1025@gmail.com>
2021-03-12 18:28:55 -0500

    Index -- Bug Fixing
    If you ever read this you can see how big of a fucking idiot I was...

--- global/authorization_check.php
+++ global/authorization_check.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "fishbpzf_admin", "fisersenigma", "login");
+$conn = mysqli_connect("localhost", "fishbpzf_admin", "fishersenigma", "login");
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
