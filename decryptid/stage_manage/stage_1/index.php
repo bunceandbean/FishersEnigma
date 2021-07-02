405553d4da93f61a7e72e6fdb075b23301822c74
Ben Lilley <benlilley1025@gmail.com>
2021-07-01 23:28:50 -0400

    Changing .MOV Format

--- decryptid/stage_manage/stage_1/index.php
+++ decryptid/stage_manage/stage_1/index.php
@@ -24,7 +24,7 @@ if( !(authorize(1)) ){
     <article class="">
       <section>
         <!-- <p>1-623-269-4600</p> -->
-        <video src="enigma_s1.MOV" autoplay loop>
+        <video src="enigma_s1.mp4" autoplay loop>
         </video>
       </section>
     </article>
